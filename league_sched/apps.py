0fe7363443072aef100a10a48a882eb845c641c1
Ryan Quinn <fowardingtoyou@gmail.com>
2016-07-21 17:35:59 -0400

    Initial commit

---
+++ league_sched/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LeagueSchedConfig(AppConfig):
+    name = 'league_sched'
---
+++ league_sched/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    sign_up_date = models.DateTimeField()
+    password = models.CharField(max_length=64)
+    karma = models.DecimalField()
+
+
+class LeagueName(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Check if unique id is given so we don't have to worry about name changes (someone else owning the name)
+    league_name = models.CharField(max_length=30)
+    verified = models.BooleanField(default=0)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    tag = models.CharField(max_length=5)
+    # Probably has to be stored separately (RD and actual rating)
+    rating = models.DecimalField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    team_karma = models.DecimalField()
+
+
+class TeamMembers(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+
+class FutureMatch(models.Model):
+    match_date = models.DateTimeField()
+    # Probably shouldn't allow people to delete teams if one is planned. Than delete and inform manually
+    team_1 = models.ForeignKey(Team)
+    team_2 = models.ForeignKey(Team)
+    # "R" or "B"
+    side = models.CharField(max_length=1)
+
+
+class CompletedMatch(models.Model):
+    match_date = models.DateTimeField()
+    # Allow matches to exist in history even if team is deleted
+    team_1 = models.ForeignKey(Team, db_constraint=False)
+    team_2 = models.ForeignKey(Team, db_constraint=False)
+    # Which side was red to begin
+    starting_team = models.ForeignKey(Team, db_constraint=False)
