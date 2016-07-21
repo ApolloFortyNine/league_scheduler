04d86c04f2a3a4f60e26041337394daadb04e3c4
Ryan Quinn <fowardingtoyou@gmail.com>
2016-07-21 19:09:18 -0400

    Added view to see, but not modify or add, users.

--- league_sched/views.py
+++ league_sched/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from league_sched.models import User
+from league_sched.serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
--- league_scheduler/settings.py
+++ league_scheduler/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'league_sched',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
