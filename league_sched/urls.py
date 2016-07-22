0ee5e1b65e45b675a4fa98e382f0d54071db3d77
Ryan Quinn <fowardingtoyou@gmail.com>
2016-07-22 00:03:42 -0400

    Converted all code to the correct API format (all generics where possible)

--- league_sched/urls.py
+++ league_sched/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from league_sched import views
 
 urlpatterns = [
-    url(r'^users/$', views.user_list),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
-    url(r'^league_names/(?P<pk>[0-9]+)/$', views.league_name_detail),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^league_names/(?P<pk>[0-9]+)/$', views.LeagueNameDetail.as_view()),
 ]
--- league_sched/views.py
+++ league_sched/views.py
@@ -1,78 +1,109 @@
-from rest_framework import status
+from rest_framework import status, generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from league_sched.models import User, LeagueName
 from league_sched.serializers import UserSerializer, LeagueNameSerializer
 
 
-@api_view(['GET', 'POST'])
-def user_list(request, format=None):
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+###########################
+# The graveyard belows serves as a reminder as to why you should always read
+# the entire tutorial before starting a project
+###########################
 
-    elif request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST'])
+# def user_list(request, format=None):
+#     if request.method == 'GET':
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def user_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a user instance.
-    """
-    try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def league_name_detail(request, pk, format=None):
-    if request.method == 'POST':
-        serializer = LeagueNameSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def user_detail(request, pk, format=None):
+#     """
+#     Retrieve, update or delete a user instance.
+#     """
+#     try:
+#         user = User.objects.get(pk=pk)
+#     except User.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'PUT':
+#         serializer = UserSerializer(user, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == 'DELETE':
+#         user.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    try:
-        league_name = LeagueName.objects.get(pk=pk)
-    except LeagueName.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        league_name = LeagueName.objects.get(pk=pk)
-        serializer = LeagueNameSerializer(league_name)
-        return Response(serializer.data)
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    elif request.method == 'PUT':
-        serializer = LeagueNameSerializer(league_name, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST', 'PUT', 'DELETE'])
+# def league_name_detail(request, pk, format=None):
+#     if request.method == 'POST':
+#         serializer = LeagueNameSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     try:
+#         league_name = LeagueName.objects.get(pk=pk)
+#     except LeagueName.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         league_name = LeagueName.objects.get(pk=pk)
+#         serializer = LeagueNameSerializer(league_name)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'PUT':
+#         serializer = LeagueNameSerializer(league_name, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == 'DELETE':
+#         league_name.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    elif request.method == 'DELETE':
-        league_name.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+###################
+# Mixins example
+###################
+
+# class LeagueNameDetail(generics.GenericAPIView,
+#                        mixins.RetrieveModelMixin):
+#     queryset = LeagueName.objects.all()
+#     serializer_class = LeagueNameSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+
+class LeagueNameDetail(generics.RetrieveUpdateDestroyAPIView,
+                       generics.CreateAPIView):
+    queryset = LeagueName.objects.all()
+    serializer_class = LeagueNameSerializer
