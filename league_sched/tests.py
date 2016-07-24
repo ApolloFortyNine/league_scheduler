336f799db1344ca456b63e72fca76eef895b6687
Ryan Quinn <fowardingtoyou@gmail.com>
2016-07-24 18:29:25 -0400

    Added a test case

--- league_sched/tests.py
+++ league_sched/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from league_sched.models import User
 
-# Create your tests here.
+class AndrewTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username="killself", karma=5.0, password="negative", sign_up_date="2016-07-21T18:50:58.434343Z")
+
+    def test_animals_can_speak(self):
+        test1 = User.objects.get(username="killself")
+        self.assertEqual(test1.username, 'killself', msg="Received name {0}".format(test1.username))
